9f3e7614e52852a46d839aba4bbd16e236c6f917
Abusha <abusha8134@gmail.com>
2025-04-17 20:26:31 +0300

    function

---
+++ function.py
@@ -0,0 +1,46 @@
+# def square(a):
+#     print(a*a)
+#     return "hi"
+
+
+# print(square(2))
+#next
+# def greeting():
+#     print('Hello World')
+
+
+# greeting()
+# greeting()
+# greeting()
+
+#next
+# def hello():
+#     return "Hello World"
+
+# print(hello())
+
+# next
+# def greeting(text):
+#     print(f'Hello, {text}')
+
+
+# greeting("sung")
+# greeting("Naruto")
+
+#next
+# def hobbies(*hobbie):
+#     print('My hobbies:')
+#     for i in hobbie:
+#         print("   -", i)
+
+
+# hobbies("driving", "codding", "movies")
+
+#next
+def information(name, age, phone):
+    print(f'Name: {name}')
+    print(f'Age: {age}')
+    print(f'Phone Number: {phone}')
+
+
+information('Abrham', 19, '0963210848')
